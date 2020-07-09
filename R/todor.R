5249c92ceb13a4fe9857c8b10b80da78cfe87efd
Dominik Krzeminski <dominik@appsilon.com>
2020-07-09 12:58:12 +0100

    instead of ugly error it throws nice modal dialog when not in project now

--- R/todor.R
+++ R/todor.R
@@ -136,9 +136,12 @@ todor_file <- function(file_name, todo_types = NULL) {
 todor_project_addin <- function() {
   project_path <- rstudioapi::getActiveProject()
   if (is.null(project_path))
-    stop(paste0("You're not within R project. Change to project, ",
-                "or use `todor_file` instead."))
-  todor(search_path = project_path)
+    rstudioapi::showDialog("TODOr",
+      paste0("You're not within R project. Change to project, ",
+                  "or use `todor_file` instead.")
+    )
+  else
+    todor(search_path = project_path)
 }
 
 #' Todor package addin
